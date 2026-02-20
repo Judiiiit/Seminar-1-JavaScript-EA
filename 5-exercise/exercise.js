279f97a0f9211f64b1077cb9806fd1e7517bbc92
Judiiiit <juditmartinez.2803@gmail.com>
2026-02-20 12:25:37 +0100

    ejercicio hecho

--- 5-exercise/exercise.js
+++ 5-exercise/exercise.js
@@ -11,9 +11,44 @@ fetch('https://jsonplaceholder.typicode.com/users/')
       // YOUR CODE STARTS HERE
       console.log("--- Processed Users ---");
       // 1. Filter even IDs
+      console.log("FILTER:")
+      const evenUsers = users.filter(({ id }) => id % 2 === 0);
+      console.log(evenUsers)
+
+
       // 2. Map to clean objects {id, name, city}
+      console.log("MAP: User info: id, name and city (all user ids):")
+      const cleanAllUsers = users.map(({ id, name, address: { city } }) => ({
+          id,
+          name,
+          city
+      }));
+      console.log(cleanAllUsers)
+
+      const cleanEvenUsers = evenUsers.map(({ id, name, address: { city } }) => ({
+          id,
+          name,
+          city
+      }));
+      console.log("MAP: User info: id, name and city (only even ids):")
+      console.log(cleanEvenUsers)
+
+
       // 3. Add Guest User at the start using Spread (...)
-      
+      console.log("ADD GUEST USER:")
+      const finalUsers = [
+          { id: 0, name: "Guest User", city: "Unknown" },
+          ...cleanAllUsers
+      ];
+      console.log(finalUsers);
+
+
       console.log("--- Statistics ---");
       // 4. Reduce to count total characters in names
+      console.log("REDUCE: ")
+      const totalCharacters = users.reduce((total, { username }) => {
+          return total + username.length;
+      }, 0);
+
+      console.log("Total characters in all usernames:", totalCharacters);
   });
